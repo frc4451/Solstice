6c0fc41ced78b0c616409eca2fa06fa54c35232a
Lewis Arias <lewisgarias@outlook.com>
2024-05-30 18:38:02 -0400

    nix: OpenCV is now built with GTK3 for the local demo

--- flake.nix
+++ flake.nix
@@ -9,31 +9,49 @@
     systems = ["x86_64-linux" "aarch64-linux"];
     eachSystem = lib.genAttrs systems;
 
-    # Done very similarly to https://github.com/hyprwm/Hyprland/blob/main/flake.nix
-    pkgsFor = eachSystem (system: inputs.nixpkgs.legacyPackages.${system});
+    pkgsFor = eachSystem (system:
+      import inputs.nixpkgs {
+        inherit system;
+
+        overlays = [
+          (final: prev: {
+            opencv4 = prev.opencv4.override {
+              enableGtk3 = true;
+            };
+
+            python312Packages =
+              prev.python312Packages
+              // {
+                opencv4 = final.opencv4.override {
+                  enablePython = true;
+                  pythonPackages = final.python312Packages;
+                };
+              };
+          })
+        ];
+      });
   in {
     formatter = eachSystem (system: inputs.nixpkgs.legacyPackages.${system}.alejandra);
 
     devShells = eachSystem (system: let
       pkgs = pkgsFor.${system};
+      pythonWithPackages = pkgs.python312.withPackages (ps:
+        with ps; [
+          blinker
+          click
+          itsdangerous
+          jinja2
+          markupsafe
+          numpy
+          opencv4
+          pillow
+          # setuptools
+          werkzeug
+          # wheel
+        ]);
     in {
-      default = pkgs.mkShellNoCC {
-        nativeBuildInputs = let
-          requirements = ps:
-            with ps; [
-              blinker
-              click
-              itsdangerous
-              jinja2
-              markupsafe
-              numpy
-              opencv4
-              pillow
-              # setuptools
-              werkzeug
-              # wheel
-            ];
-        in [(pkgs.python312.withPackages requirements)];
+      default = pkgs.mkShell {
+        packages = [pythonWithPackages];
       };
     });
   };
