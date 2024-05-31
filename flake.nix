d08f3c563db7583b5496cf53aee1705ec3e89146
Lewis Arias <lewisgarias@outlook.com>
2024-05-30 20:18:52 -0400

    Nix: Add gstreamer deps

--- flake.nix
+++ flake.nix
@@ -40,9 +40,29 @@
           werkzeug
           # wheel
         ]);
+
+      # https://wiki.nixos.org/wiki/GStreamer
+      gstreamerPackages = with pkgs; [
+          # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+          gst_all_1.gstreamer
+          # Common plugins like "filesrc" to combine within e.g. gst-launch
+          gst_all_1.gst-plugins-base
+          # Specialized plugins separated by quality
+          gst_all_1.gst-plugins-good
+          # gst_all_1.gst-plugins-bad
+          # gst_all_1.gst-plugins-ugly
+          # Plugins to reuse ffmpeg to play almost every video format
+          # gst_all_1.gst-libav
+          # Support the Video Audio (Hardware) Acceleration API
+          gst_all_1.gst-vaapi
+          # ...
+        ];
     in {
       default = pkgs.mkShell {
-        packages = [pythonWithPackages];
+        packages = [pythonWithPackages] ++ gstreamerPackages;
+        shellHook = ''
+          export GST_V4L2_USE_LIBV4L2=1
+        '';
       };
     });
   };
